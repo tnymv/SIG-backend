3f1b3bf2fa0263b5bf49df6629e56fd33db67094
Wilver Emilio Xiá Ixcot <125522983+WilverEmilio@users.noreply.github.com>
2025-09-15 22:06:31 -0600

    Agregando la conexion con la base de datos en postgresSQL

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, APIRouter
+from starlette.responses import RedirectResponse
+from app.db.database import engine, Base
+
+app = FastAPI(
+    title="API SIG Backend",
+    description=(
+        "API para el Backend de SIG.\n\n"
+        "Esta API proporciona endpoints para gestionar y acceder a las funcionalidades principales "
+        "del Sistema de Información Geográfica (SIG). Incluye características para la administración "
+        "de la base de datos, autenticación de usuarios, recuperación de datos e integración con otros servicios. "
+        "Está diseñada para ser escalable y segura, sirviendo como la base del SIG y soportando tanto clientes internos "
+        "como externos. Además, facilita la gestión eficiente de la información geoespacial, permitiendo operaciones "
+        "de consulta, actualización y análisis de datos espaciales, así como la interoperabilidad con otras plataformas."
+    ),
+    version="1.0.0"
+)
+
+api_version = APIRouter(prefix="/api/v1")
+
+# Aqui tienen que ir todos los endpoints de la version 1
+
+app.include_router(api_version)
+
+@app.on_event("startup")
+async def startup():
+    Base.metadata.create_all(bind=engine)
+        
+@app.on_event("shutdown")
+async def shutdown():
+    print("Conexión a la base de datos cerrada")
+    
+@app.get('/')
+async def inicio():
+    return RedirectResponse(url='/docs/')
--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv  import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:123456789@localhost:5432/prueba_mvc?client_encoding=utf8&sslmode=disable"
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está configurada en el entorno")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
