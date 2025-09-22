4654fe8c8837239fcb0acb5a4668d9d7f1e08b87
Carlos Cano <cardan@MacBook-Air-de-Carlos.local>
2025-09-21 18:33:37 -0600

    Cambios de Username a User 2.0

--- app/controllers/User/user.py
+++ app/controllers/User/user.py
@@ -21,7 +21,7 @@ async def get_user(
         offset = (page - 1) * limit
         if page < 1 or limit < 1:
             raise HTTPException(status_code=400, detail="La página y el límite deben ser mayores que 0")
-        usernames = db.query(User).offset(offset).limit(limit).all()
+        usernames = db.query(Username).offset(offset).limit(limit).all()
         return success_response([
             UserResponse.model_validate(user).model_dump(mode="json")
             for user in usernames
@@ -31,7 +31,7 @@ async def get_user(
 
 @router.post('',response_model = UserResponse)
 async def create_user(user_data: UserBase, db: Session = Depends(get_db)):
-    if db.query(User).filter(User.user == user_data.user).first():
+    if db.query(Username).filter(Username.user == user_data.user).first():
         raise HTTPException(
             status_code=409,
             detail=existence_response_dict(True, "El user ya existe"),
@@ -41,7 +41,7 @@ async def create_user(user_data: UserBase, db: Session = Depends(get_db)):
     hashed_password = get_password_hash(user_data.password_hash)
     
     try:
-        new_user = User(
+        new_user = Username(
             user=user_data.user,
             password_hash=hashed_password,
             employee_id=user_data.employee_id,
