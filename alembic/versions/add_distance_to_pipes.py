c67c5f09cf953b403819959537a2f544b62e6938
Edwin Mejia <programador.mn@ciancoders.dev>
2025-12-07 22:22:56 -0600

    fix: Correct down_revision in add_distance_to_pipes Alembic migration.

--- alembic/versions/add_distance_to_pipes.py
+++ alembic/versions/add_distance_to_pipes.py
@@ -14,7 +14,7 @@ from sqlalchemy import inspect
 
 # revision identifiers, used by Alembic.
 revision: str = 'add_distance_pipes'
-down_revision: Union[str, None] = '676567411805'
+down_revision: Union[str, None] = '59292210906c'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
